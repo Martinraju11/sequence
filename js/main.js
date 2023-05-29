aca14b9a0466b78a721d5bbfa27ecfe85198d713
Martinraju11 <martinraju53@gmail.com>
2023-05-29 19:30:05 +0530

    update

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var n=parseInt(prompt("Enter the limit of sequence of numbers"));
+document.write("Sequence of numbers is");
+	document.write("<br>");
+for(let i=0;i<=n;i++)
+	
+{
+	document.write(i);
+	document.write("<br>");
+}
